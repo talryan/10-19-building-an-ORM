970e8a67db9c55576db668ebac6955e9b2ba4705
talryan <nataliamryan@gmail.com>
2020-11-20 11:57:31 -0800

    Created Author class

--- config/environment.rb
+++ config/environment.rb
@@ -11,5 +11,7 @@ DB[:conn].results_as_hash = true
 
 require_relative '../lib/tweet'
 require_relative '../db/seed'
+require_relative '../lib/author'
 
 Tweet.create_table 
+Author.create_table
--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
-class Tweet 
+class Author
 
-    attr_accessor :content, :author_id, :id
+    attr_accessor :name, :id
 
     def initialize(attr_hash={})
         #is NOT responsible for sending attributes to the db
@@ -9,20 +9,20 @@ class Tweet
             if self.respond_to?("#{key.to_s}=")
                 self.send("#{key.to_s}=", value)
             end
-            #self.content=(value)
+            #self name=(value)
         end
-        #{content: Faker::Quote.matz, author_id: 'Matz'}
+        # name: Faker::Quote.matz, author: 'Matz'}
     end
 
     def self.all
-       array_of_hashes = DB[:conn].execute("SELECT * FROM tweets")
+       array_of_hashes = DB[:conn].execute("SELECT * FROM authors")
        array_of_hashes.collect do |hash|
          self.new(hash)
        end
     end
 
     def self.find(id)
-        sql = "SELECT * from tweets WHERE tweets.id = ?"
+        sql = "SELECT * from authors WHERE authors.id = ?"
         obj_hash = DB[:conn].execute(sql, id)[0]
         self.new(obj_hash)
     end
@@ -35,18 +35,18 @@ class Tweet
         if !!self.id  
              # if it is already saved, update it 
              sql = <<-SQL 
-                UPDATE tweets
-                SET content = ?, author_id = ?
+                UPDATE authors
+                SET name = ?
                 WHERE id = ?;
              SQL
-             DB[:conn].execute(sql, self.content, self.author_id, self.id)
+             DB[:conn].execute(sql, self.name, self.id)
         else
             # if it is not already saved, add to db
             sql = <<-SQL 
-                INSERT INTO tweets (content, author_id) 
-                VALUES (?,?)
+                INSERT INTO authors (name)
+                VALUES (?)
             SQL
-            DB[:conn].execute(sql, @content, self.author_id)
+            DB[:conn].execute(sql, self.name)
             @id = DB[:conn].last_insert_row_id
         end
         self
@@ -55,17 +55,18 @@ class Tweet
     def self.create_table 
         # responsible for creating a class
         sql = <<-SQL
-            CREATE TABLE IF NOT EXISTS tweets (
+            CREATE TABLE IF NOT EXISTS authors (
                 id INTEGER PRIMARY KEY,
-                content TEXT,
-                author_id INTEGER
+             name TEXT
             )
         SQL
 
         DB[:conn].execute(sql)
     end
 
-    def author
-        Author.find(self.author_id)
+    def tweets
+        Tweet.all.select do |tweet|
+            tweet.author_id == self.id
+        end
     end
 end
